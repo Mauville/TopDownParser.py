49c87ee641b238ebdb8a548a360aa8195a3c4457
Mauville <4991127@outlook.com>
2020-10-11 23:37:21 -0500

    Implement minimize()

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
+import string
+
+
 class State:
     def __init__(self, name: str, isInitial=False, isFinal=False):
         self.name = name
@@ -13,6 +16,76 @@ class State:
 
     def setFinal(self):
         self.isFinal = True
+
+    def __eq__(self, other):
+        # don't attempt to compare against unrelated types
+        if not isinstance(other, State):
+            return NotImplemented
+        return self.name == other.name and self.links == other.links and self.isFinal == other.isFinal and self.isInitial == other.isInitial
+
+
+class Automaton:
+    """ Contains nodes representing an automaton"""
+
+    def __init__(self, states, alphabet, initial_state, final_states, str_states):
+        # All lists of strings except the states, which are States
+        self.states = states
+        self.alphabet = alphabet
+        self.initial_state = initial_state
+        self.final_states = final_states
+        self.str_states = str_states
+
+    def update_strings(self):
+        """ Updates all the string values with graph values. Usually after minimization."""
+
+        # Clear string values for update
+        self.initial_state = []
+        self.final_states = []
+        self.str_states = []
+
+        # Update with values of graph
+        for each_state in self.states:
+            if each_state.isInitial:
+                self.initial_state = each_state.name
+            if each_state.isFinal:
+                self.final_states.append(each_state.name)
+            self.str_states.append(each_state.name)
+
+    def minimize(self):
+        """Create a table from the states and minimize it"""
+
+        table = []
+        for each_state in self.states:
+            table.append(each_state.links)
+        # begin loop counting matches
+        seen = []
+        duplicate = []
+        for row in table:
+            if row in seen and row not in duplicate:
+                duplicate.append(row)
+            else:
+                seen.append(row)
+
+        # if match minimize, else return ourselves untouched
+        if duplicate:
+            new_state_ids = list(string.ascii_lowercase)
+            for duplicate_state in duplicate:
+                # rename all states matches q0 to qA
+                current_letter = new_state_ids.pop()
+                for this_states in self.states:
+                    if this_states.links == duplicate_state:
+                        # BUG If more than 24 reduction states, will break.
+                        this_states.name = "q" + current_letter
+            #  remove duplicates
+            seen = []
+            for each_state in self.states:
+                if each_state not in seen:
+                    seen.append(each_state)
+            self.states = seen
+
+        self.update_strings()
+
+        return self
 
 
 def dataReader(filename="test1.txt"):
@@ -42,11 +115,10 @@ def dataReader(filename="test1.txt"):
                     if destination.name == final_state:
                         partialstate.train(string, destination)
 
+    return Automaton(states, alphabet, initial_state, final_states, read_states)
 
-#TODO implement automaton
-#TODO refactor to factory
-#TODO implement string validation
-#TODO implement DFA minimization
-#TODO create menu for string validation
-#TODO ask instructor for how to take in values? is this a purely CLI program?
 
+a = dataReader().minimize()
+print("a")
+
+# TODO implement menu
